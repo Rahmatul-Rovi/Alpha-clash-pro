729054b0131c08d28e394adb346ca9056dbfae26
Rahmatul-Rovi <rahmatul.rovi@gmail.com>
2024-11-24 00:09:37 +0600

    hidden home screen and added the play screen

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,21 @@
+// function play(){
+//   //Hide the home screen.. hide the home screen to add the class  hidden to the home section
+//   const homeSection = document.getElementById('home-screen');
+//   homeSection.classList.add('hidden');
+  
+//   //show the playground
+//   const playgroundSection = document.getElementById('play-ground');
+//   playgroundSection.classList.remove('hidden');
+// }
+
+
+function continueGame(){
+//step-1 generate a random alphabet
+
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame()
+}
